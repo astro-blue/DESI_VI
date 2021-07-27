8c1648c7ae5600643b27be987ccba362894492ec
astro-blue <tingwenlan@gmail.com>
2021-07-27 02:48:22 -0700

    Update

---
+++ Everest_assessment/my_plot_style.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plt
+def my_plot_style():
+   
+    params = {'legend.fontsize': 20,
+             'axes.labelsize': 20,
+             'axes.titlesize':20,
+             'xtick.labelsize':20,
+             'ytick.labelsize':20,
+             'xtick.major.size':5,
+              'xtick.minor.size':2.5,
+             'ytick.major.size':5,
+              'ytick.minor.size':2.5,
+             'figure.facecolor':'w',
+             #'lines.linewidth' : 1.5,
+              'xtick.major.width':1.5,
+              'ytick.major.width':1.5,
+              'xtick.minor.width':1.5,
+              'ytick.minor.width':1.5,
+              'axes.linewidth':1.5,
+              'xtick.direction':'in',
+              'ytick.direction':'in',
+             'ytick.labelleft':True,
+              'text.usetex' : False,
+             'font.family': 'sans-serif'}
+  
+    plt.rcParams.update(params)
+    
+def removing_ytick_label():
+    ax = plt.gca()
+    ax.axes.yaxis.set_ticklabels([])
+    
